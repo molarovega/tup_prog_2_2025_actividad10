e8934929cb735cbd83fa14f3685a41b25d5946a4
molarovega <molarofrancisco@hotmail.com>
2025-10-23 16:06:36 -0300

    ejercicio 1

---
+++ Guia 10/Ejercicio1/Models/Solicitud.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+
+    [Serializable]
+
+    public class Solicitud: IExportable
+    {
+        public int Numero { get; set; }
+        public string Motivo { get; set; }
+
+        public void Importar(String datos)
+        {
+            string[] campos = datos.Split(';');
+            Numero = Convert.ToInt32(campos[0]);
+            Motivo = campos[1];
+        }
+
+        override public string ToString()
+        {
+            return $"{Numero}({Motivo})";
+        }
+    }
+}
